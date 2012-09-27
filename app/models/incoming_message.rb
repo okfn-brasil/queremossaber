6c6637b3ba87f078f22ef5f7033b27341772bea9
Louise Crow <louise.crow@gmail.com>
2012-09-27 11:25:43 +0100

    Move the added mail attribute accessors to the mail parsing modules.

--- app/models/incoming_message.rb
+++ app/models/incoming_message.rb
@@ -40,15 +40,6 @@ require 'zip/zip'
 require 'mapi/msg'
 require 'mapi/convert'
 
-# Monkeypatch! Adding some extra members to store extra info in.
-module TMail
-    class Mail
-        attr_accessor :url_part_number
-        attr_accessor :rfc822_attachment # when a whole email message is attached as text
-        attr_accessor :within_rfc822_attachment # for parts within a message attached as text (for getting subject mainly)
-    end
-end
-
 class IncomingMessage < ActiveRecord::Base
     belongs_to :info_request
     validates_presence_of :info_request
--- lib/mail_parsing_with_tmail.rb
+++ lib/mail_parsing_with_tmail.rb
@@ -1,3 +1,13 @@
+# Monkeypatch! Adding some extra members to store extra info in.
+module TMail
+    class Mail
+        attr_accessor :url_part_number
+        attr_accessor :rfc822_attachment # when a whole email message is attached as text
+        attr_accessor :within_rfc822_attachment # for parts within a message attached as text (for getting subject mainly)
+    end
+end
+
+
 module MailParsingWithTmail
 
     def MailParsingWithTmail.mail_from_raw_email(data)
