7d26aaab777a9075ef3c69f0038c602f22222f0f
Louise Crow <louise.crow@gmail.com>
2012-11-14 13:21:05 +0000

    Add function to get a tnef message as a Mail Mail, rather than TMail Mail.

--- lib/tnef.rb
+++ lib/tnef.rb
@@ -2,14 +2,10 @@ require 'tmpdir'
 
 class TNEF
 
-    # Extracts all attachments from the given TNEF file as a TMail::Mail object
-    # The TNEF file also contains the message body, but in general this is the
-    # same as the message body in the message proper.
-    def self.as_tmail(content)
-        main = TMail::Mail.new
-        main.set_content_type 'multipart', 'mixed', { 'boundary' => TMail.new_boundary }
+    def self.get_attachments(content)
+        attachments = []
         Dir.mktmpdir do |dir|
-            IO.popen("#{`which tnef`.chomp} -K -C #{dir}", "w") do |f|
+            IO.popen("#{`which tnef`.chomp} -K -C #{dir}", "wb") do |f|
                 f.write(content)
                 f.close
                 if $?.signaled?
@@ -22,11 +18,9 @@ class TNEF
             found = 0
             Dir.new(dir).sort.each do |file| # sort for deterministic behaviour
                 if file != "." && file != ".."
-                    file_content = File.open("#{dir}/#{file}", "r").read
-                    attachment = TMail::Mail.new
-                    attachment['content-location'] = file
-                    attachment.body = file_content
-                    main.parts << attachment
+                    file_content = File.open("#{dir}/#{file}", "rb").read
+                    attachments << { :content => file_content,
+                                     :filename => file }
                     found += 1
                 end
             end
@@ -34,6 +28,30 @@ class TNEF
                 raise IOError, "tnef produced no attachments"
             end
         end
+        attachments
+    end
+
+    # Extracts all attachments from the given TNEF file as a TMail::Mail object
+    # The TNEF file also contains the message body, but in general this is the
+    # same as the message body in the message proper.
+    def self.as_tmail(content)
+        main = TMail::Mail.new
+        main.set_content_type 'multipart', 'mixed', { 'boundary' => TMail.new_boundary }
+        get_attachments(content).each do |attachment|
+            tmail_attachment = TMail::Mail.new
+            tmail_attachment['content-location'] = attachment[:filename]
+            tmail_attachment.body = attachment[:content]
+            main.parts << tmail_attachment
+        end
+        main
+    end
+
+    def self.as_mail(content)
+        main = Mail.new
+        get_attachments(content).each do |attachment|
+            main.add_file(attachment)
+        end
+        main.ready_to_send!
         main
     end
 
